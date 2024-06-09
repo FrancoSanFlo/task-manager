94d28fed954bae55f513cbd639622037ce7b3822
FrancoSanFlo <sanchezfranco222@gmail.com>
2024-06-09 16:39:29 -0400

    fix de header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,12 @@
 import PlusIcon from "./PlusIcon";
 import Button from "./Button";
 
-const Header = ({handleButtonClick}) => {
-
+const Header = ({ handleButtonClick }) => {
   return (
     <header className="w-full p-3 bg-blue-950 flex flex-row items-center justify-between">
       <h1 className="text-white text-2xl sm:text-3xl font-semibold">
         Task Manager
       </h1>
-      {/* Desktop */}
-      <Button
-        text="+ Tarea"
-        className={
-          "text-white text-md py-1 px-4 bg-black/35 rounded-md hover:bg-black/50 transition ease-in-out duration-300 mt-2 sm:mt-0 sm:block hidden"
-        }
-        onButtonClick={handleButtonClick}
-      />
-      {/* Mobile */}
-      <Button
-        className={
-          "text-white p-1 flex items-center justify-center bg-black/35 rounded-full hover:bg-black/50 transition ease-in-out duration-300 mt-2 sm:mt-0 w-8 h-8 sm:hidden"
-        }
-        onButtonClick={handleButtonClick}
-      >
-        <PlusIcon />
-      </Button>
     </header>
   );
 };
--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -1,16 +1,24 @@
-import OverviewCard from "./OverviewCard"
+import OverviewCard from "./OverviewCard";
 
 const SideMenu = ({ tasks }) => {
-  const pendingTasks = tasks.filter((task) => !task.done)
-  const finishedTasks = tasks.filter((task) => task.done)
+  const pendingTasks = tasks.filter((task) => !task.done);
+  const finishedTasks = tasks.filter((task) => task.done);
 
   return (
     <div className="w-full h-auto sm:w-[30%] grid grid-cols-2 grid-rows-2 sm:grid-rows-4">
       <OverviewCard title="Total" tasks={tasks} className={"col-span-2"} />
-      <OverviewCard title="Pending" tasks={pendingTasks} className={"col-span-1 sm:col-span-2 md:col-span-1"} />
-      <OverviewCard title="Finished" tasks={finishedTasks} className={"col-span-1 sm:col-span-2 md:col-span-1"} />
+      <OverviewCard
+        title="Pending"
+        tasks={pendingTasks}
+        className={"col-span-1 sm:col-span-2 md:col-span-1"}
+      />
+      <OverviewCard
+        title="Finished"
+        tasks={finishedTasks}
+        className={"col-span-1 sm:col-span-2 md:col-span-1"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
